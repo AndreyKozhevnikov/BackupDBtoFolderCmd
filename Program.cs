8ab589171ecfb82d897dc40bfed279eb94ab5f6a
Andrey <freazeek@mail.ru>
2020-01-21 21:32:09 +0300

    use sql directly

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using DophinMSSQLConnector;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,19 +10,17 @@ namespace BackupDBtoFolderCmd {
     class Program {
         
         static void Main(string[] args) {
-            //StreamReader sr = new StreamReader(@"C:\MSSQLSettings.ini");
-            //string st = sr.ReadToEnd();
-            //sr.Close();
-            //MsSqlConnector.GetSettingsFromTxt(st);
-            MsSqlConnector.MsServerName = @"(localdb)\mssqllocaldb";
-            MsSqlConnector.MsIntegratedSecurity = true; 
-            MsSqlConnector.Open();
+            string sConn = string.Format(@"Data Source={0};Integrated Security=True", @"(localdb)\mssqllocaldb");
+            SqlConnection connSql = new SqlConnection(sConn);
+            connSql.Open();
+            SqlCommand commSql = connSql.CreateCommand();
             string baseName = args[0];
             string backupPath = args[1];
-            string backupName = string.Format("{0}\\[{1}].bak", backupPath, baseName);
+            string backupName = string.Format("{0}\\{1}.bak", backupPath, baseName);
             string sqlQuery = String.Format("BACKUP DATABASE [{0}] TO  DISK = '{1}' WITH NOFORMAT, NOINIT,  NAME = N'{0}-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10", baseName, backupName);
-            MsSqlConnector.MakeNonQuery(sqlQuery);
-            MsSqlConnector.Close();
+            commSql.CommandText = sqlQuery;
+            commSql.ExecuteNonQuery();
+            connSql.Close();
         }
     }
 }
